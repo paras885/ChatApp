04732ec5c85f0a5140bf42e23dc9218ba504600e
Paras Meena <paras.meena885@gmail.com>
2016-08-18 00:50:19 +0530

    now

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var http = require('http').Server(app);
 var ios = require('socket.io')(http);
 var users = {};
 
+app.set('port', (process.env.PORT || 5000));
+
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
- });
+  	res.sendFile(__dirname + '/index.html');
+ }).listen(app.get('port'), function() {
+	console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 var users = {};
 
@@ -27,8 +31,4 @@ ios.on('connection', function(socket){
   });
 
 });
-
-http.listen(4000, '0.0.0.0', function(){
-  console.log('listening on *:4000');
-  });
 
